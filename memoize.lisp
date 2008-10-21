aa7e02f40ed3c6813651c7444067439439ef828a
Ramarren <ramarren@gmail.com>
2008-10-21 21:20:34 +0200

    Make memoize? memoize only, try to write left-recursion separately

--- memoize.lisp
+++ memoize.lisp
@@ -2,6 +2,15 @@
 
 (defun memoize? (p)
   "Create identical, but memoized, parser"
+  (let ((memo-table (make-hash-table)))
+    #'(lambda (inp)
+	(let ((result (gethash inp memo-table :first-time-called)))
+	  (if (eql result :first-time-called)
+	      (copy-list (setf (gethash inp memo-table)
+			       (funcall p inp)))
+	      (copy-list result))))))
+
+(defun left-recursive? (p)
   (let ((memo-table (make-hash-table))
 	(count-table (make-hash-table))
 	(length-table (make-hash-table)))
@@ -9,11 +18,12 @@
 	(unless (gethash inp length-table)
 	  (setf (gethash inp length-table) (length inp)))
 	(incf (gethash inp count-table 0))
-	(if (> (1+ (gethash inp count-table))
-	       (gethash inp length-table))
+	(if (> (print (1+ (gethash inp count-table)))
+	       (print (gethash inp length-table)))
 	    nil
 	    (let ((result (gethash inp memo-table :first-time-called)))
 	      (if (eql result :first-time-called)
 		  (copy-list (setf (gethash inp memo-table)
 				   (funcall p inp)))
-		  (copy-list result)))))))
+		  (copy-list result))))))
+  )
