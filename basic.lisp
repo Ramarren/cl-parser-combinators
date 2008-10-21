afaddc28be068cf1da620db55a1e90f41bb43f98
Ramarren <ramarren@gmail.com>
2008-10-21 15:29:21 +0200

    Mame basic defparser macro

--- basic.lisp
+++ basic.lisp
@@ -6,16 +6,17 @@
   ((tree :accessor tree-of :initarg :tree :initform nil)
    (suffix :accessor suffix-of :initarg :suffix :initform nil)))
 
-(defun result (v inp)
-  (list (make-instance 'parser-possibility :tree v :suffix inp)))
+(defun result (v)
+  #'(lambda (inp)
+      (list (make-instance 'parser-possibility :tree v :suffix inp))))
 
-(defun zero (inp)
-  (declare (ignore inp))
-  nil)
+(defun zero ()
+  (constantly nil))
 
-(defun item (inp)
-  (when inp
-    (list (make-instance 'parser-possibility :tree (car inp) :suffix (cdr inp)))))
+(defun item ()
+  #'(lambda (inp)
+      (when inp
+	(list (make-instance 'parser-possibility :tree (car inp) :suffix (cdr inp))))))
 
 ;;; emulating monads... did I even understand those?
 ;;; bind      :: Parser a -> (a -> Parser b) -> Parser b
@@ -32,10 +33,10 @@
 	    (nconcing (funcall (funcall parser-generator v) inp-prime)))))
 
 (defun sat (predicate)
-  (bind #'item #'(lambda (x)
+  (bind (item) #'(lambda (x)
 		   (if (funcall predicate x)
-		       (curry #'result x)
-		       #'zero))))
+		       (result x)
+		       (zero)))))
 
 (defun char? (character)
   (sat (curry #'eql character)))
@@ -64,5 +65,30 @@
   (let ((ne-word (bind (letter?)
 		       #'(lambda (x)
 			   (bind (word?) #'(lambda (xs)
-					     (curry #'result (cons x xs))))))))
-    (choice ne-word (curry #'result nil))))
+					     (result (cons x xs))))))))
+    (choice ne-word (result nil))))
+
+;;; spec as in monad comprehensions, result variable parser variable parser...
+(defmacro defparser (name input &body parser-spec)
+  (labels ((recursive-part (input &rest parser-spec)
+	     (cond ((null parser-spec)
+		    `(result ,input))
+		   (t (destructuring-bind (variable parser . remain) parser-spec
+			`(bind ,parser
+			       #'(lambda (,variable)
+				   ,@(when (eql variable '_)
+				       (list `(declare (ignore ,variable))))
+				   ,(apply #'recursive-part input remain))))))))
+    `(defun ,name (,input)
+       ,(apply #'recursive-part input parser-spec))))
+
+(defparser %string? character-list
+  _ (char? (car character-list))
+  _ (string? (cdr character-list)))
+
+(defun string? (character-list)
+  (let ((string-parser (%string? character-list))
+	(result-nil (result nil)))
+    (if (null character-list)
+	result-nil
+	string-parser)))
