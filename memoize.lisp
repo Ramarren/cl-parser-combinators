b94bb0d8b7449fd0d1d8be9d8aabf81b3223c03b
Ramarren <ramarren@gmail.com>
2008-10-22 12:58:10 +0200

    This thing doesn't handle recursion at all anyway.

--- memoize.lisp
+++ memoize.lisp
@@ -3,27 +3,34 @@
 (defparameter *memo-table* (make-hash-table))
 
 (defun memoize? (p label)
-  "Create identical, but memoized, parser"
+  "Create identical, but memoized, parser."
   (unless (gethash label *memo-table*)
     (setf (gethash label *memo-table*) (make-hash-table)))
-  #'(lambda (inp)
-      (let ((memo-table (gethash :label *memo-table*)))
+  (let ((memo-table (gethash :label *memo-table*)))
+    #'(lambda (inp)
 	(multiple-value-bind (result result-p) (gethash inp memo-table)
 	  (if result-p
 	      (copy-list result)
 	      (copy-list (setf (gethash inp memo-table)
 			       (funcall p inp))))))))
 
-(defun left-recursive (p alt)
-  (let ((count-table (make-hash-table))
-	(length-table (make-hash-table)))
-    (labels ((recur (inp)
-	       (unless (gethash inp length-table)
-		 (setf (gethash inp length-table) (length inp)))
-	       (incf (gethash inp count-table 0))
-	       (if (> (1+ (gethash inp count-table))
-		      (gethash inp length-table))
-		   (funcall alt inp)
-		   (funcall (mdo #'recur p)
-			    inp))))
-      #'recur)))
+(defparameter *curtail-table* (make-hash-table))
+
+(defun curtail? (p label)
+  (unless (gethash label *curtail-table*)
+    (setf (gethash label *curtail-table*) (make-hash-table)))
+  (let ((curtail-table (gethash :label *curtail-table*)))
+    (labels ((curtailed (inp)
+	       (multiple-value-bind (counter counter-p) (gethash inp curtail-table)
+		 (cond (counter-p
+			(destructuring-bind (c . l) counter
+			  (cond ((> c (1+ l))
+				 nil)
+				(t
+				 (incf (car counter))
+				 (funcall p inp)))))
+		       (t
+			(setf (gethash inp curtail-table)
+			      (cons 1 (length inp)))
+			(funcall p inp))))))
+      #'curtailed)))
