5969ef5649ca5fb8ca25b3d4875abdd03b0981d2
Ramarren <ramarren@gmail.com>
2008-10-22 12:02:17 +0200

    Implement memoization with explicit labels
    This solves problems with memoizing recursive calls, and parser sharing, and so on.

--- memoize.lisp
+++ memoize.lisp
@@ -1,14 +1,18 @@
 (in-package :parser-combinator)
 
-(defun memoize? (p)
+(defparameter *memo-table* (make-hash-table))
+
+(defun memoize? (p label)
   "Create identical, but memoized, parser"
-  (let ((memo-table (make-hash-table)))
-    #'(lambda (inp)
-	(let ((result (gethash inp memo-table :first-time-called)))
-	  (if (eql result :first-time-called)
+  (unless (gethash label *memo-table*)
+    (setf (gethash label *memo-table*) (make-hash-table)))
+  #'(lambda (inp)
+      (let ((memo-table (gethash :label memo-table)))
+	(multiple-value-bind (result result-p) (gethash inp memo-table)
+	  (if result-p
+	      (copy-list result)
 	      (copy-list (setf (gethash inp memo-table)
-			       (funcall p inp)))
-	      (copy-list result))))))
+			       (funcall p inp))))))))
 
 (defun left-recursive (p alt)
   (let ((count-table (make-hash-table))
