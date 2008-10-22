ffbb6c9dfb070d41668468f9494decbdc51325a1
Ramarren <ramarren@gmail.com>
2008-10-22 15:36:12 +0200

    Use delay/force in memoize

--- memoize.lisp
+++ memoize.lisp
@@ -5,32 +5,32 @@
   (unless (gethash label *memo-table*)
     (setf (gethash label *memo-table*) (make-hash-table)))
   (let ((memo-table (gethash label *memo-table*)))
-    #'(lambda ()
-	(let ((p (funcall promise)))
-	  #'(lambda (inp)
-	      (multiple-value-bind (result result-p) (gethash inp memo-table)
-		(if result-p
-		    (copy-list result)
-		    (copy-list (setf (gethash inp memo-table)
-				     (funcall p inp))))))))))
+    (delay
+      (let ((p (force promise)))
+	#'(lambda (inp)
+	    (multiple-value-bind (result result-p) (gethash inp memo-table)
+	      (if result-p
+		  (copy-list result)
+		  (copy-list (setf (gethash inp memo-table)
+				   (funcall p inp))))))))))
 
 (defun curtail? (promise label)
   (unless (gethash label *curtail-table*)
     (setf (gethash label *curtail-table*) (make-hash-table)))
   (let ((curtail-table (gethash label *curtail-table*)))
-    #'(lambda ()
-	(let ((p (funcall promise)))
-	  (labels ((curtailed (inp)
-		     (multiple-value-bind (counter counter-p) (gethash inp curtail-table)
-		       (cond (counter-p
-			      (destructuring-bind (c . l) counter
-				(cond ((>= c (1+ l))
-				       nil)
-				      (t
-				       (incf (car counter))
-				       (funcall p inp)))))
-			     (t
-			      (setf (gethash inp curtail-table)
-				    (cons 1 (length inp)))
-			      (funcall p inp))))))
-	    #'curtailed)))))
+    (delay
+      (let ((p (force promise)))
+	(labels ((curtailed (inp)
+		   (multiple-value-bind (counter counter-p) (gethash inp curtail-table)
+		     (cond (counter-p
+			    (destructuring-bind (c . l) counter
+			      (cond ((>= c (1+ l))
+				     nil)
+				    (t
+				     (incf (car counter))
+				     (funcall p inp)))))
+			   (t
+			    (setf (gethash inp curtail-table)
+				  (cons 1 (length inp)))
+			    (funcall p inp))))))
+	  #'curtailed)))))
