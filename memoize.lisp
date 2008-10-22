57956309ab26cf2132768e263aeb54b9b5a4c92c
Ramarren <ramarren@gmail.com>
2008-10-22 14:00:13 +0200

    Update memoize and curtail for promises

--- memoize.lisp
+++ memoize.lisp
@@ -2,35 +2,40 @@
 
 (defparameter *memo-table* (make-hash-table))
 
-(defun memoize? (p label)
-  "Create identical, but memoized, parser."
+(defun memoize? (promise label)
+  "Create identical, but memoized, parser promise."
   (unless (gethash label *memo-table*)
     (setf (gethash label *memo-table*) (make-hash-table)))
   (let ((memo-table (gethash :label *memo-table*)))
-    #'(lambda (inp)
-	(multiple-value-bind (result result-p) (gethash inp memo-table)
-	  (if result-p
-	      (copy-list result)
-	      (copy-list (setf (gethash inp memo-table)
-			       (funcall p inp))))))))
+    #'(lambda ()
+	(let ((p (funcall promise)))
+	  #'(lambda (inp)
+	      (multiple-value-bind (result result-p) (gethash inp memo-table)
+		(if result-p
+		    (copy-list result)
+		    (copy-list (setf (gethash inp memo-table)
+				     (funcall p inp))))))))))
 
 (defparameter *curtail-table* (make-hash-table))
 
-(defun curtail? (p label)
+(defun curtail? (promise label)
   (unless (gethash label *curtail-table*)
     (setf (gethash label *curtail-table*) (make-hash-table)))
-  (let ((curtail-table (gethash :label *curtail-table*)))
-    (labels ((curtailed (inp)
-	       (multiple-value-bind (counter counter-p) (gethash inp curtail-table)
-		 (cond (counter-p
-			(destructuring-bind (c . l) counter
-			  (cond ((> c (1+ l))
-				 nil)
-				(t
-				 (incf (car counter))
-				 (funcall p inp)))))
-		       (t
-			(setf (gethash inp curtail-table)
-			      (cons 1 (length inp)))
-			(funcall p inp))))))
-      #'curtailed)))
+  (let ((curtail-table (gethash label *curtail-table*)))
+    #'(lambda ()
+	(let ((p (funcall promise)))
+	  (labels ((curtailed (inp)
+		     (multiple-value-bind (counter counter-p) (gethash inp curtail-table)
+		       (cond (counter-p
+			      (destructuring-bind (c . l) counter
+				(print l)
+				(cond ((>= c (1+ l))
+				       nil)
+				      (t
+				       (incf (car counter))
+				       (funcall p inp)))))
+			     (t
+			      (setf (gethash inp curtail-table)
+				    (cons 1 (length inp)))
+			      (funcall p inp))))))
+	    #'curtailed)))))
