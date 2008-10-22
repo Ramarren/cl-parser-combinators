8c59ac0b2bfe2f927dfca276e85031c84b3d6817
Ramarren <ramarren@gmail.com>
2008-10-22 16:47:37 +0200

    Export basic functions/macros

--- package.lisp
+++ package.lisp
@@ -1,2 +1,14 @@
 (defpackage :parser-combinator
-    (:use :cl :iterate :alexandria :bpm))
+    (:use :cl :iterate :alexandria :bpm)
+  (:export #:def-cached-parser
+	   #:def-memo1-parser
+	   #:result
+	   #:zero
+	   #:item
+	   #:sat
+	   #:choice
+	   #:choice1
+	   #:choices
+	   #:choices1
+	   #:mdo
+	   #:parse-string))
