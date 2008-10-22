3f13e24e52d728de782d4456d848cd349b5f39f3
Ramarren <ramarren@gmail.com>
2008-10-22 13:48:40 +0200

    Macroize combinators for added laziness.

--- basic.lisp
+++ basic.lisp
@@ -7,16 +7,19 @@
    (suffix :accessor suffix-of :initarg :suffix :initform nil)))
 
 (defun result (v)
-  #'(lambda (inp)
-      (list (make-instance 'parser-possibility :tree v :suffix inp))))
+  #'(lambda ()
+     #'(lambda (inp)
+	 (list (make-instance 'parser-possibility :tree v :suffix inp)))))
 
 (defun zero ()
-  (constantly nil))
+  #'(lambda ()
+      (constantly nil)))
 
 (defun item ()
-  #'(lambda (inp)
-      (when inp
-	(list (make-instance 'parser-possibility :tree (car inp) :suffix (cdr inp))))))
+  #'(lambda ()
+      #'(lambda (inp)
+	  (when inp
+	    (list (make-instance 'parser-possibility :tree (car inp) :suffix (cdr inp)))))))
 
 ;;; emulating monads... did I even understand those?
 ;;; bind      :: Parser a -> (a -> Parser b) -> Parser b
@@ -24,13 +27,16 @@
 ;;; p ‘bind‘ f = \inp -> concat [f v inp’ | (v,inp’) <- p inp]
 ;;; (bind p f inp)=(concat list-comprehension)
 
-
-(defun bind (parser parser-generator)
-  #'(lambda (inp)
-      (iter (for possibility in (funcall parser inp))
-	    (for v = (tree-of possibility))
-	    (for inp-prime = (suffix-of possibility))
-	    (nconcing (funcall (funcall parser-generator v) inp-prime)))))
+;;; emulate laziness as well, otherwhise any sort of recursion fails hard
+(defmacro bind (parser-promise parser-promise-generator) ; results in parser-promise
+  `#'(lambda ()
+       (let ((parser-promise ,parser-promise)
+	     (parser-promise-generator ,parser-promise-generator))
+	 #'(lambda (inp)
+	     (iter (for possibility in (funcall (funcall parser-promise) inp))
+		   (for v = (tree-of possibility))
+		   (for inp-prime = (suffix-of possibility))
+		   (nconcing (funcall (funcall (funcall parser-promise-generator v)) inp-prime)))))))
 
 (defun sat (predicate)
   (bind (item) #'(lambda (x)
@@ -38,31 +44,39 @@
 		       (result x)
 		       (zero)))))
 
-(defun choice (parser1 parser2)
-  #'(lambda (inp)
-      (nconc (funcall parser1 inp)
-	     (funcall parser2 inp))))
+(defmacro choice (parser1-promise parser2-promise)
+  `#'(lambda ()
+       (let ((parser1-promise ,parser1-promise)
+	     (parser2-promise ,parser2-promise))
+       #'(lambda (inp)
+	   (nconc (funcall (funcall parser1-promise) inp)
+		  (funcall (funcall parser2-promise) inp))))))
 
-(defun choice1 (parser1 parser2)
-  #'(lambda (inp)
-      (let ((results1 (funcall parser1 inp)))
-	(if results1
-	    (list (car results1))
-	    (let ((results2 (funcall parser2 inp)))
-	      (when results2
-		(list (car results2))))))))
+(defmacro choice1 (parser1-promise parser2-promise)
+  `#'(lambda ()
+       (let ((parser1-promise ,parser1-promise)
+	     (parser2-promise ,parser2-promise))
+	 #'(lambda (inp)
+	     (let ((results1 (funcall (funcall parser1-promise) inp)))
+	       (if results1
+		   (list (car results1))
+		   (let ((results2 (funcall (funcall parser2-promise) inp)))
+		     (when results2
+		       (list (car results2))))))))))
 
-(defun choices (&rest parser-list)
-  (if (cdr parser-list)
-      (choice (car parser-list)
-	      (apply #'choices (cdr parser-list)))
-      (car parser-list)))
+(defmacro choices (&rest parser-promise-list)
+  (if (cdr parser-promise-list)
+      `(choice ,(car parser-promise-list)
+	       (choices ,@(cdr parser-promise-list)))
+      (car parser-promise-list)))
 
-(defun choices1 (&rest parser-list)
-  #'(lambda (inp)
-      (iter (for p in parser-list)
-	    (for result-list = (funcall p inp))
-	    (finding (list (car result-list)) such-that result-list))))
+(defmacro choices1 (&rest parser-promise-list)
+  `#'(lambda ()
+       (let ((parser-promise-list (list ,@parser-promise-list)))
+	 #'(lambda (inp)
+	     (iter (for p in parser-promise-list)
+		   (for result-list = (funcall (funcall p) inp))
+		   (finding (list (car result-list)) such-that result-list))))))
 
 ;;; here parser spec is list of (pattern optional-guard comprehension)
 ;;; using do-like notation, <- is special
@@ -108,4 +122,4 @@
   (_predicate (mdo (<- x (item)) (if (funcall _predicate x) (result x) (zero)))))
 
 (defun parse-string (parser string)
-  (mapcar #'tree-of (funcall parser (coerce string 'list))))
+  (mapcar #'tree-of (funcall (funcall parser) (coerce string 'list))))
