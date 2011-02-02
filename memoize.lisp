00e826a4d45c4d3cbf20d63bbb3160c61f07f3d2
Ramarren <ramarren@gmail.com>
2011-02-02 08:25:58 +0100

    In memoize?, move global variable management inside the closure.

--- memoize.lisp
+++ memoize.lisp
@@ -1,15 +1,17 @@
 (in-package :parser-combinators)
 
 (defun memoize? (parser &optional (label (gensym)))
-  "Create identical, but memoized, parser."
-  (unless (gethash label *memo-table*)
-    (setf (gethash label *memo-table*) (make-hash-table)))
-  (let ((memo-table (gethash label *memo-table*)))
+  "Parser modifier: memoizes the parser, which if called again at a given position it will return a result
+immediately. LABEL is used for memoization key, use explicit one if the parser is used in multiple places."
+  (with-parsers (parser)
     #'(lambda (inp)
-        (multiple-value-bind (result result-p) (gethash (position-of inp) memo-table)
-          (let ((new-result (if result-p
-                                (copy-parse-result result)
-                                (copy-parse-result (setf (gethash (position-of inp) memo-table)
-                                                         (make-parse-result (funcall parser inp)))))))
-            #'(lambda ()
-                (next-result new-result)))))))
+        (unless (gethash label *memo-table*)
+          (setf (gethash label *memo-table*) (make-hash-table)))
+        (let ((memo-table (gethash label *memo-table*)))
+          (multiple-value-bind (result result-p) (gethash (position-of inp) memo-table)
+            (let ((new-result (if result-p
+                                  (copy-parse-result result)
+                                  (copy-parse-result (setf (gethash (position-of inp) memo-table)
+                                                           (make-parse-result (funcall parser inp)))))))
+              #'(lambda ()
+                  (next-result new-result))))))))
